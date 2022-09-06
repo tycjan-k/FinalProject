12b4f6d22ac786f5d61a95640f4ee82fb55d3f9e
Tycjan Korzyk <tycjankorzyk@gmail.com>
2022-09-06 20:02:04 +0200

    first

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required
+
+import time
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def index():
+    return render_template("index.html")
