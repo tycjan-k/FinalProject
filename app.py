307fc8e8bea056fa67cc3ba42a0771197fe3e157
Tycjan Korzyk <tycjankorzyk@gmail.com>
2022-09-09 16:46:34 +0200

    register option

--- app.py
+++ app.py
@@ -16,6 +16,52 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///fp.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 @app.route("/")
-def index():
-    return render_template("index.html")
+def home():
+    return render_template("home.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    #User reached route via POST (clicking on a button with a post method)
+    if request.method == "POST":
+
+        #Require username
+        if not request.form.get("username"):
+            return false
+        #Look if username is taken and apologize if it is
+        lookusname = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        if len(lookusname) != 0:
+            return false
+        else:
+            username = request.form.get("username")
+
+        #Require password
+        if not request.form.get("password"):
+            return false
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        if password != confirmation:
+            return false
+        else:
+            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, generate_password_hash(password))
+
+        return redirect("/")
+    return render_template("register.html")
